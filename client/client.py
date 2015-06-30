22d8c6ca829be131b998328ff4a37a9e540cbabb
Chris Barry <chris@barry.im>
2015-06-29 21:19:42 -0400

    Add --debug flag, add --i2p-directory flag, print error when i2pcontrol fails to connect.

--- client/client.py
+++ client/client.py
@@ -39,6 +39,8 @@ def print_entry(ent):
 	
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
+	parser.add_argument('-d', '--debug', help='prints request json instead of sending',type=bool, default=False)
+	parser.add_argument('-i', '--i2p-directory', help='I2P home',type=str, default='~/.i2p/')
 	parser.add_argument('-s', '--server', help='where to send data',type=str, default='tuuql5avhexhn7oq4lhyfythxejgk4qpavxvtniu3u3hwfwkogmq.b32.i2p')
 	parser.add_argument('-p', '--port', help='where to send data',type=int, default='80')
 	parser.add_argument('-t', '--token', help='token to prove yourself',type=str, default='')
@@ -46,29 +48,40 @@ if __name__ == '__main__':
 
 	if not args.token:
 		print 'Use a token. See --help for usage.'
-		raise SystemExit, 0
+		raise SystemExit, 1
 
 	rpc = i2py.control.pyjsonrpc.HttpClient(
 		url = ''.join(['http://',args.server,':',str(args.port)]),
 		gzipped = True
 	)
-	
-	# NetDB Stuff
-	i2py.netdb.inspect(hook=print_entry)
 
 	# Local router stuff
-	a = i2py.control.I2PController()
-	vals = a.get_router_info()
+	try:
+		a = i2py.control.I2PController(address=('127.0.0.1',9999))
+	except:
+		print 'I2PControl not installed, or router is down.'
+		raise SystemExit, 1
+
+	ri_vals = a.get_router_info()
+	
 	this_router = {
-		'activepeers'          : vals['i2p.router.netdb.activepeers'],
-		'fastpeers'            : vals['i2p.router.netdb.fastpeers'],
-		'highcapacitypeers'    : vals['i2p.router.netdb.highcapacitypeers'],
-		'tunnelsparticipating' : vals['i2p.router.net.tunnels.participating'],
+		'activepeers'          : ri_vals['i2p.router.netdb.activepeers'],
+		'fastpeers'            : ri_vals['i2p.router.netdb.fastpeers'],
+		'highcapacitypeers'    : ri_vals['i2p.router.netdb.highcapacitypeers'],
+		'tunnelsparticipating' : ri_vals['i2p.router.net.tunnels.participating'],
+		'netDb.writePending'   : a.get_rate(stat='netDb.writePending', period=3600), # TODO: Send more info
 	}
+	
+	# NetDB Stuff
+	i2py.netdb.inspect(hook=print_entry,netdb_dir=args.i2p_directory)
 
 	try:
-		rpc.collect(token=args.token, netdb=routers, local=this_router, version=VERSION)
+		if args.debug:
+			# To check the approximate size of a request, run this. No network call is sent. Results in bytes.
+			# $ python client.py | gzip --stdout | wc --bytes
+			print i2py.control.pyjsonrpc.create_request_json("collect", token=args.token, netdb=routers, local=this_router, version=VERSION)
+		else:
+			rpc.collect(token=args.token, netdb=routers, local=this_router, version=VERSION)
 	except i2py.control.pyjsonrpc.JsonRpcError, err:
 		print 'Error code {}: {} -- {}'.format(err.code, err.message, err.data)
 		
-
